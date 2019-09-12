99d106364634dace39e3d4aae02c4f261069cfa4
Matyliano <joanna.boszko@gmail.com>
2019-09-12 19:35:30 +0200

    adding SpringBoot dependecies & changing controller

---
+++ src/main/java/matyliano/task_pg/TaskPgApplication.java
@@ -0,0 +1,14 @@
+package matyliano.task_pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPgApplication.class, args);
+    }
+
+
+}
--- src/main/java/matyliano/task_pg/controller/ExchangeController.java
+++ src/main/java/matyliano/task_pg/controller/ExchangeController.java
@@ -1,4 +1,4 @@
-package controller;
+package matyliano.task_pg.controller;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -20,24 +20,41 @@ public class ExchangeController {
 
         RestTemplate restTemplate = new RestTemplate();
 
-         String exchangeUrl =  "http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json";
+        String exchangeUrl =  "http://api.nbp.pl/api/exchangerates/rates/c/usd/today/";
 
-         ResponseEntity<String> exchangeResponse = restTemplate.getForEntity(exchangeUrl,String.class);
+        ResponseEntity<String> exchangeResponse = restTemplate.getForEntity(exchangeUrl,String.class);
 
-         String  mid = exchangeResponse.getBody();
+        String  mid = exchangeResponse.getBody();
+
 
         StringBuffer sentenceBuilder = new StringBuffer();
-        JsonParser jsonParser = new JsonParser();
-        JsonElement baseJsonElement = jsonParser.parse(mid);
-        JsonObject baseJsonObject = baseJsonElement.getAsJsonObject();
-        JsonArray ratesJsonArray = baseJsonObject.get("rates").getAsJsonArray();
-        JsonObject ratesJsonObject = ratesJsonArray.get(0).getAsJsonObject();
-        String rates = ratesJsonObject.get("mid").getAsString();
-        sentenceBuilder.append(rates);
+        StringBuffer sentenceBuilder2 = new StringBuffer();
+        StringBuffer sentenceBuilder3 = new StringBuffer();
 
+
+        JsonParser jsonParser = new JsonParser();
+
+        JsonElement baseJsonElement = jsonParser.parse(mid);
+
+        JsonObject baseJsonObject = baseJsonElement.getAsJsonObject();
+
+        JsonArray ratesJsonArray = baseJsonObject.get("rates").getAsJsonArray();
+
+        JsonObject ratesJsonObject = ratesJsonArray.get(0).getAsJsonObject();
+
+        String bid = ratesJsonObject.get("bid").getAsString();
+        String ask = ratesJsonObject.get("ask").getAsString();
+        String effectiveDate = ratesJsonObject.get("effectiveDate").getAsString();
+
+        sentenceBuilder.append(bid);
+        sentenceBuilder2.append(ask);
+        sentenceBuilder3.append(effectiveDate);
 
         ModelAndView modelAndView = new ModelAndView("data");
-        modelAndView.addObject("usd", sentenceBuilder);
+        modelAndView.addObject("bid", sentenceBuilder);
+        modelAndView.addObject("ask", sentenceBuilder2);
+        modelAndView.addObject("effectiveDate", sentenceBuilder3);
+
         return modelAndView;
     }
 
